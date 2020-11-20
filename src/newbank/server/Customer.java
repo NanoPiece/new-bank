61086b33a80fd634c953301ca82cd1c50c5ed29f
NanoPiece <54646479+NanoPiece@users.noreply.github.com>
2020-11-20 13:58:20 +0000

    Added a new getAccount() function for newBank to be able to retrieve a particular account of a customer by pair-matching account name.
    This is implemented due to the need of changeMyAccountName functionality.

--- src/newbank/server/Customer.java
+++ src/newbank/server/Customer.java
@@ -57,4 +57,13 @@ public class Customer {
 	public void addAccount(Account account) {
 		accounts.add(account);		
 	}
+
+	public Account getAccount(String accountName) {
+		for(Account a : accounts){
+			if (a.getAccountName().equals(accountName)) {
+				return a;
+			}
+		}
+		return null;
+	}
 }
